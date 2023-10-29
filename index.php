a5f9e5bd69c58181a8e71fc823c42b74b45fb795
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2023-10-29 11:25:46 -0300

    header

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
     <section class="w-full relative block shadow-lg shadow-slate-600" id="hero">
-      <img src="<?php bloginfo("template_directory");?>/img/hero.jpg" alt="Flores" class="object-fill">
+      <img src="<?php bloginfo("template_directory");?>/img/hero.jpg" alt="Flores" class="object-cover">
     </section>
 <?php get_footer(); ?>
