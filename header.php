d316e81ba8a0fda06d82d018f7e1110b11bca789
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2023-10-29 11:17:58 -0300

    header

--- header.php
+++ header.php
@@ -80,7 +80,7 @@
 
 </head>
 <body>
-    <header class="flex flex-row bg-white flex-1 w-full h-40 justify-between items-center py-6 px-10 bg-[url('<?php bloginfo('template_directory')?>/img/bgtexture.jpg'] bg-cover">  
+    <header class="flex flex-row bg-white flex-1 w-full h-40 justify-between items-center py-6 px-10 bg-[url('<?php bloginfo('template_directory')?>/img/bgtexture.jpg')] bg-cover">  
           <img src="<?php bloginfo('template_directory')?>/img/logo-cultivo-patagonia.png" alt="Cultivo Patagonia">
           <div class="rss flex flex-row items-center justify-center">
             <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -100,6 +100,6 @@
           </div>
     </header>
 
-    <nav class="bg-amber-100 py-4 px-10 flex flex-row items-center justify-center">
+    <nav class="bg-[#F2EEEE]py-4 px-10 flex flex-row items-center justify-center">
             <?php wp_nav_menu( array('menu' => 'mainmenu' )); ?>
     </nav>
