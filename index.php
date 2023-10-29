35474d078f322481f944190faa1ef0d034f19ab2
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2023-10-29 12:28:56 -0300

    header btn

--- index.php
+++ index.php
@@ -44,7 +44,14 @@
       </ul>
     </section>
 
-    <section>
-
+    <section id="gallery" class="flex w-full my-10 p-10">
+      <div class="grid grid-cols-5 grid-rows-5 gap-4">
+          <div class="bg-[#F2EEEE] col-span-2 row-span-5">1</div>
+          <div class="bg-[#F2EEEE] col-span-3 row-span-2 col-start-3">2</div>
+          <div class="bg-[#F2EEEE] row-span-3 col-start-3 row-start-3">3</div>
+          <div class="bg-[#F2EEEE] row-span-3 col-start-4 row-start-3">4</div>
+          <div class="bg-[#F2EEEE] row-span-3 col-start-5 row-start-3">5</div>
+      </div>
     </section>
+
 <?php get_footer(); ?>
