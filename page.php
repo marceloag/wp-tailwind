f96feabe7bf8bed8cbe45cafec02d8d921dc4a64
Marcelo Javier Aguila <marceloag@Mac-mini-de-Marcelo.local>
2023-11-22 11:08:40 -0300

    Single Page

--- page.php
+++ page.php
@@ -2,9 +2,9 @@
 
 	  <section>
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <div class="flex flex-row items-center justify-start" id="interior">
-            <div class="w-10/12 my-3">
-                <div class="titulo">
+        <div class="flex flex-row items-center justify-center" id="interior">
+            <div class="w-10/12">
+                <div class="titulo mx-3">
                     <h1><?php the_title();?></h1>
                 </div>
                 <div class="ml-4">
